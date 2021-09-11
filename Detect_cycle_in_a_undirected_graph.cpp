020c5d178c3ddd8ba2164c5632ead1d7d3dfa4cb
git5v <79326074+git5v@users.noreply.github.com>
2021-09-11 19:32:34 +0530

    Create Detect_cycle_in_a_undirected_graph.cpp

---
+++ Detect_cycle_in_a_undirected_graph.cpp
@@ -0,0 +1,26 @@
+bool solve(int node,int parent,vector<int>adj[],vector<int>&vis)
+    {
+        vis[node]=1;
+        for(auto x:adj[node])
+        {
+            if(!vis[x])
+            {
+                if(solve(x,node,adj,vis)) return true;
+            }
+            else if(x!=parent) return true;
+        }
+        return false;
+    }
+    
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        vector<int>vis(V,0);
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            {
+                if(solve(i,-1,adj,vis)) return true;
+            }
+        }
+        return false;
+    }
