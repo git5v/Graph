6e0f1f1fe7eadc89655bb0b690870bb2c36a5469
Vedant Mukhedkar <79326074+git5v@users.noreply.github.com>
2022-06-26 17:52:08 +0530

    Create Connected_Components_in_an_undirected_graph.cpp

---
+++ Connected_Components_in_an_undirected_graph.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    private:
+    void dfs(int i,vector<vector<int>> &adj,vector<int> &vis)
+    {
+        vis[i] = 1;
+        for(int ii=0;ii<adj[i].size();ii++)
+        {
+            if(!vis[ii] and adj[ii][i]) dfs(ii,adj,vis);  // check if not visited and matrix correspondings are also marked as 1
+        }
+    }
+  public:
+    int numProvinces(vector<vector<int>> adj, int V) {
+        
+        vector<int> vis(V,0);
+        int c = 0 ;
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i]) {c++; dfs(i,adj,vis);}
+        }
+        return c;
+    }
