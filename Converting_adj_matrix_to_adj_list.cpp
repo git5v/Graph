6a44b9f1372f8fd58e9b40e749b9084c23e486ab
Vedant Mukhedkar <79326074+git5v@users.noreply.github.com>
2022-06-27 19:53:13 +0530

    Create Converting_adj_matrix_to_adj_list.cpp

---
+++ Converting_adj_matrix_to_adj_list.cpp
@@ -0,0 +1,22 @@
+
+void fun(vector<vector<int>> &matrix)
+{
+  int n = matrix.size();
+  vector<pair<int,int>> adj_list[n+1];
+  
+  for(auto &it:matrix)
+  {
+    adj_list[it[0]].emplace_back(make_pair(t[1],t[2])); 
+            //t[0]-> parent node, t[1]->child nodes, t[2]->weigths   
+  }
+   //For printing adj list
+  for(int i=0;i<n;i++)
+  {
+    cout<<i->;
+    for(auto &it:adj_list[i])
+      cout<<it<< " ";
+  }
+  cout<<endl;
+  }
+
+ }
